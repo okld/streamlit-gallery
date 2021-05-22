2ca1da81dc16931cd0d982a94c1c60bf7f26573f
okld <65978885+okld@users.noreply.github.com>
2021-05-22 05:08:59 +0200

    Update gallery & pandas profiling app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,13 @@
-import requests
-import inspect
 import streamlit as st
 import streamlit_pages as pages
 
 from pathlib import Path
 
 PAGES = {
-    "Home": {
-        "readme": None,
-        "source": lambda: st.markdown(Path("README.md").read_text()),
-    },
-    "Ace Editor": {
-        "readme": "https://raw.githubusercontent.com/okld/streamlit-ace/master/README.md",
-        "source": pages.ace,
-    },
-    "Elements": {
-        "readme": "https://raw.githubusercontent.com/okld/streamlit-elements/main/README.md",
-        "source": pages.elements,
-    },
-    "Pandas Profiling": {
-        "readme": "https://raw.githubusercontent.com/okld/streamlit-pandas-profiling/master/README.md",
-        "source": pages.pandas_profiling,
-    }
+    "Home": lambda: st.markdown(Path("README.md").read_text()),
+    "Ace Editor": pages.ace,
+    "Elements": pages.elements,
+    "Pandas Profiling": pages.pandas_profiling,
 }
 
 
@@ -40,22 +26,9 @@ def main():
         page_name = st.radio("", page_names, page_default)
         st.write("---")
     
-    page = PAGES[page_name]
-    body = st.beta_container()
-
-    if page_name != "Home":
-        st.write("---")
-
-        with st.beta_expander("README"):
-            st.markdown(requests.get(page["readme"]).text)
-    
-        with st.beta_expander("SOURCE"):
-            st.code(Path(inspect.getsourcefile(page["source"])).read_text())
-    
-    with body:
-        page["source"]()
+    PAGES[page_name]()
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Showcase", layout="wide")
+    st.set_page_config(page_title="Okld Gallery", layout="wide")
     main()
--- streamlit_pages/pandas_profiling/app.py
+++ streamlit_pages/pandas_profiling/app.py
@@ -1,21 +1,31 @@
+import requests
 import pandas as pd
+import pandas_profiling
 import streamlit as st
 
-from pandas_profiling import ProfileReport
+from pathlib import Path
 from streamlit_pandas_profiling import st_profile_report
 
 
 def main():
+    st.markdown(requests.get("https://raw.githubusercontent.com/okld/streamlit-pandas-profiling/master/README.md").text)
+
     dataset = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
 
     df = pd.read_csv(dataset)
-    pr = ProfileReport(df, explorative=True)
+    pr = df.profile_report(explorative=True)
 
     st.write(df)
     st.sidebar.write(f"🔗 [Titanic dataset]({dataset})")
 
     with st.beta_expander("REPORT", expanded=True):
         st_profile_report(pr)
+    
+    with st.beta_expander("USAGE"):
+        st.help(st_profile_report)
+    
+    with st.beta_expander("SOURCE"):
+        st.code(Path(__file__).read_text())
 
 
 if __name__ == "__main__":
